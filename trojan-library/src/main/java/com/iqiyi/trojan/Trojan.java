7b78fd21f07257afb7b8c6b4c06347dca0b61a63
Xiaofei <xiaofei.it@gmail.com>
2017-10-17 15:27:13 +0800

    First commit

---
+++ trojan-library/src/main/java/com/iqiyi/trojan/Trojan.java
@@ -0,0 +1,15 @@
+package com.iqiyi.trojan;
+
+/**
+ * Created by Xiaofei on 2017/10/17.
+ */
+
+public class Trojan {
+    public static void onEnterMethod(String clazzName, String methodName, Object target, Object[] parameters) {
+
+    }
+
+    public static void config() {
+
+    }
+}
