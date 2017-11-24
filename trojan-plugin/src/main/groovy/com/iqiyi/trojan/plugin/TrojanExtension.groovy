4fb551e8573a8507ae0b43247cb901fb0017ec13
Desert <zyallday@gmail.com>
2017-11-24 11:45:30 +0800

    feature add update: upgrade gradle 2.14.1 --> gradle 4.1 update: upgrade gradle build plugin 2.2.3 --> 3.0.1 feature: to enable/disable trojan by gradle.properties [trojan-plugin.enabled=true/false]
    Signed-off-by: Desert <zyallday@gmail.com>

---
+++ trojan-plugin/src/main/groovy/com/iqiyi/trojan/plugin/TrojanExtension.groovy
@@ -0,0 +1,15 @@
+
+package com.iqiyi.trojan.plugin
+
+class TrojanExtension {
+
+  def isEnabled
+
+  def isEnabled() {
+    return isEnabled
+  }
+
+  void setEnabled(isEnabled) {
+    this.isEnabled = isEnabled
+  }
+}
