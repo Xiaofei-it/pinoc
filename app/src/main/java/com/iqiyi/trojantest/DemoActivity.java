2e02901b9344c92fb78bdcfaecec34fe824852fb
Xiaofei <xiaofei.it@gmail.com>
2017-12-02 03:19:18 +0800

    Remove useless code

--- app/src/main/java/com/iqiyi/trojantest/DemoActivity.java
+++ app/src/main/java/com/iqiyi/trojantest/DemoActivity.java
@@ -7,11 +7,6 @@ import android.widget.TextView;
 
 public class DemoActivity extends AppCompatActivity {
 
-    private int num = 0;
-    private void f(int num) {
-        this.num = num + 1;
-    }
-
     private void init() {
         Intent intent = getIntent();
         TextView textView = (TextView) findViewById(R.id.tv);
@@ -20,24 +15,7 @@ public class DemoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
-            f(savedInstanceState.getInt("num"));
-        }
         setContentView(R.layout.activity_demo);
         init();
-    }
-
-    private void initTmp() {
-        Intent intent = getIntent();
-        String tmp = intent.getStringExtra("tmp");
-        if (tmp != null) {
-            TextView textView = (TextView) findViewById(R.id.tv);
-            textView.setText(tmp.toUpperCase());
-        }
-    }
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("num", num);
     }
 }
